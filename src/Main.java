77ab0a86dd846b9066c2af9afc25d1af9a9819af
andrei-loktev <119010210+andrei-loktev@users.noreply.github.com>
2023-06-05 14:39:19 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
-        for (Integer num : nums) {
-            if (num % 2 == 1) {
+        Set<Integer> set = new HashSet<>(nums);
+        for (Integer num : set) {
+            if (num % 2 == 0) {
                 System.out.print(num + " ");
+            }
             }
         }
     }
-}
