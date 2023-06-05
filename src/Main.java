24c0af718753363b1f459af1b531a5b77670cead
andrei-loktev <119010210+andrei-loktev@users.noreply.github.com>
2023-06-05 14:54:10 +0300

    task 3

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,8 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> nums = new ArrayList<>(List.of(10, 1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
-        Set<Integer> set = new TreeSet<>(nums);
-        for (Integer num : set) {
-            if (num % 2 == 0) {
-                System.out.print(num + " ");
-            }
-        }
+        List<String> word = new ArrayList<>(Arrays.asList("zaq", "xsw", "cde", "zaq", "xsw", "bgt", "xsw"));
+        Set<String> set = new HashSet<>(word);
+        System.out.println(set);
     }
 }
