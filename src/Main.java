1e0aec90d345573aaf07a3f5663244576f0df3e8
andrei-loktev <119010210+andrei-loktev@users.noreply.github.com>
2023-06-05 14:43:17 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,13 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
-        Set<Integer> set = new HashSet<>(nums);
+        List<Integer> nums = new ArrayList<>(List.of(10, 1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        Set<Integer> set = new TreeSet<>(nums);
         for (Integer num : set) {
             if (num % 2 == 0) {
                 System.out.print(num + " ");
             }
-            }
         }
     }
+}
